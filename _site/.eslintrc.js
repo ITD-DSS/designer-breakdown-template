fad79de5670ced6083c42dd8b3d4178be8d6cb71
Jonathan A. Mitchell <ontoneio.mm@gmail.com>
2020-10-27 17:45:00 -0700

    Adds back ESLint rules

--- _site/.eslintrc.js
+++ _site/.eslintrc.js
@@ -2,25 +2,25 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    browser: true
+    browser: true,
   },
   extends: [
-    '@nuxtjs'
-    // "plugin:vue/recommended",
-    // "eslint:recommended",
-    // "prettier/vue",
-    // "plugin:prettier/recommended",
+    '@nuxtjs',
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended',
   ],
-  // rules: {
-  //   "vue/component-name-in-template-casing": ["error", "PascalCase"],
-  //   "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-  //   "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-  // },
-  // globals: {
-  //   $nuxt: true,
-  // },
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  globals: {
+    $nuxt: true,
+  },
   parserOptions: {
     parser: 'babel-eslint',
-    sourceType: 'module'
-  }
+    sourceType: 'module',
+  },
 }
